5f0c8279125f39febded4340178e3bf4f58bcf99
Prakhar-Sahu1 <Prakhar.Sahu1@ibm.com>
2025-08-17 10:04:02 +0530

    changed names

--- src/functions_component/helperFunctions.ts
+++ src/functions_component/helperFunctions.ts
@@ -1,105 +1,139 @@
 import { genericApiCall } from "./genericApiCall";
 
-
-
 /**
  * Calculates location-based emissions.
  * @customfunction
  * @namespace ENVIZI
+ * @param {CustomFunctions.Invocation} invocation Invocation object.
+ * @requiresAddress
  */
-export async function v3_location_helper(
+export async function location(
   date: string,
   country: string,
   stateProvince: string,
   powerGrid: string,
   type: string,
   value: number,
-  unit: string
+  unit: string,
+  invocation: CustomFunctions.Invocation
 ): Promise<string[]> {
+ 
   const state = stateProvince || "";
   const grid = powerGrid || "";
-  return genericApiCall("location", {
-    date,
-    country,
-    stateProvince: state,
-    powerGrid: grid,
-    type,
-    value,
-    unit,
-  });
+
+  const response=await genericApiCall(
+    "location",
+    {
+      date,
+      country,
+      stateProvince: state,
+      powerGrid: grid,
+      type,
+      value,
+      unit,
+    },
+    invocation
+  );
+
+  return response
 }
 
 /**
  * Calculates stationary source emissions.
  * @customfunction
  * @namespace ENVIZI
+ * @param {CustomFunctions.Invocation} invocation Invocation object.
+ * @requiresAddress
  */
-export async function v3_stationary_helper(
+export async function stationary(
   date: string,
   country: string,
   stateProvince: string,
   type: string,
   value: number,
-  unit: string
+  unit: string,
+  invocation
 ): Promise<string[]> {
-  return genericApiCall("stationary", { date, country, stateProvince, type, value, unit });
+  return genericApiCall(
+    "stationary",
+    { date, country, stateProvince, type, value, unit },
+    invocation
+  );
 }
 
 /**
  * Calculates fugitive emissions.
  * @customfunction
  * @namespace ENVIZI
+ * @param {CustomFunctions.Invocation} invocation Invocation object.
+ * @requiresAddress
  */
-export async function v3_fugitive_helper(
+export async function fugitive(
   date: string,
   country: string,
   stateProvince: string,
   type: string,
   value: number,
-  unit: string
+  unit: string,
+  invocation
 ): Promise<string[]> {
-  return genericApiCall("fugitive", { date, country, stateProvince, type, value, unit });
+  return genericApiCall(
+    "fugitive",
+    { date, country, stateProvince, type, value, unit },
+    invocation
+  );
 }
 
 /**
  * Calculates mobile source emissions.
  * @customfunction
  * @namespace ENVIZI
+ * @param {CustomFunctions.Invocation} invocation Invocation object.
+ * @requiresAddress
  */
-export async function v3_mobile_helper(
+export async function mobile(
   date: string,
   country: string,
   stateProvince: string,
   type: string,
   value: number,
-  unit: string
+  unit: string,
+  invocation
 ): Promise<string[]> {
-  return genericApiCall("mobile", { date, country, stateProvince, type, value, unit });
+  return genericApiCall("mobile", { date, country, stateProvince, type, value, unit }, invocation);
 }
 
 /**
  * Calculates emissions using the generic calculation endpoint.
  * @customfunction
  * @namespace ENVIZI
+ * @param {CustomFunctions.Invocation} invocation Invocation object.
+ * @requiresAddress
  */
-export async function v3_calculation_helper(
+export async function calculation(
   date: string,
   country: string,
   stateProvince: string,
   powerGrid: string,
   type: string,
   value: number,
-  unit: string
+  unit: string,
+  invocation
 ): Promise<string[]> {
   const state = stateProvince || "";
   const grid = powerGrid || "";
-  return genericApiCall("calculation", {
-    date,
-    country,
-    stateProvince: state,
-    powerGrid: grid,
-    type,
-    value,
-    unit,
-  });
+  return genericApiCall(
+    "calculation",
+    {
+      date,
+      country,
+      stateProvince: state,
+      powerGrid: grid,
+      type,
+      value,
+      unit,
+    },
+    invocation
+  );
 }
+
