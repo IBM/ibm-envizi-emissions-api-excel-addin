6c6c6e6643fbcbf29d7eb1263ff916f7973d6401
Santhosh-Ramalingam-T <santhosh.ramalingam.t@ibm.com>
2025-09-17 17:48:52 +0530

    chore: rename search helper - import functions.ts test

--- test/functions.test.ts
+++ test/functions.test.ts
@@ -1,7 +1,7 @@
 // Copyright IBM Corp. 2025
 import * as api from "../src/functions/functions"
 import { genericApiCall } from "../src/functions/generic-api-call";
-import { factorSearch } from "../src/functions/factorSeachHelper";
+import { factorSearch } from "../src/functions/factorSearchHelper";
 import { factorHelper } from "../src/functions/factorHelper";
 
 jest.mock("../src/functions/generic-api-call", () => ({
